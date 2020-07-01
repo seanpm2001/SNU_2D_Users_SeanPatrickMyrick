cec80c8bb6a9ec9010beb39e2acd90ac0a90e51c
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-30 17:17:42 -0700

    8 files at root
    The 8 root files for this project, in CSS, JPG, HTML, PNG, and PHP formats.

---
+++ Users/SeanPatrickMyrick/Traversal_TryLink.php
@@ -0,0 +1,20 @@
+ <?php
+/* Button redirect concept
+try:
+link1.html
+else:
+try:
+link2.html
+else:
+return:
+404.html
+*/
+
+/* how it works */
+// Traverse the links
+// JavaScript on user side, PHP on server side
+// Try multiple links, if all fail, return a 404
+
+// PHP code goes here
+
+?>
